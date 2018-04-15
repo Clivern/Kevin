20fb6b72e03a76fafc0aa9f80c9f43af2c657e3f
Clivern <hello@clivern.com>
2018-04-15 15:31:47 +0200

    add option model

---
+++ app/models/option.py
@@ -0,0 +1,17 @@
+"""
+Option Model
+"""
+
+from django.db import models
+
+class Option(models.Model):
+    AUTOLOAD_CHOICES = (
+        ('on', 'On'),
+        ('off', 'Off')
+    )
+
+    key = models.CharField(max_length=30, verbose_name="Key", db_index=True)
+    value = models.CharField(max_length=200, verbose_name="Value")
+    autoload = models.CharField(max_length=5, choices=AUTOLOAD_CHOICES, default="off", verbose_name="Autoload")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created_at")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated_at")
