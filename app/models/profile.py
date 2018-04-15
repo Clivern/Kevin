0a0d17566fe8bfa87ff9ae03dbcf6369e6c79d40
Clivern <hello@clivern.com>
2018-04-15 18:04:07 +0200

    Profile model updated

--- app/models/profile.py
+++ app/models/profile.py
@@ -13,8 +13,8 @@ class Profile(models.Model):
         db_index=True
     )
     access_token = models.CharField(max_length=200, verbose_name="Access_Token")
-    access_token_update_dt = models.DateTimeField(verbose_name="Access_Token_Updated_at")
     refresh_token = models.CharField(max_length=200, verbose_name="Refresh_Token")
-    refresh_token_update_dt = models.DateTimeField(verbose_name="Refresh_Token_Updated_at")
+    access_token_updated_at = models.DateTimeField(verbose_name="Access_Token_Updated_at")
+    refresh_token_updated_at = models.DateTimeField(verbose_name="Refresh_Token_Updated_at")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created_at")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated_at")
