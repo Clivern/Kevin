cc463fae775030be98b3a6c8e42e2acf845a45ad
Clivern <hello@clivern.com>
2018-04-15 15:32:37 +0200

    add endpoint meta table

--- app/models/endpoint_meta.py
+++ app/models/endpoint_meta.py
@@ -3,6 +3,16 @@ Endpoint Meta Model
 """
 
 from django.db import models
+from .endpoint import Endpoint
 
-class EndpointMeta(models.Model):
-    pass
+class Endpoint_Meta(models.Model):
+    endpoint = models.ForeignKey(
+        Endpoint,
+        on_delete=models.CASCADE,
+        verbose_name="Endpoint",
+        db_index=True
+    )
+    key = models.CharField(max_length=30, verbose_name="Key", db_index=True)
+    value = models.CharField(max_length=200, verbose_name= "Value")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created_at")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated_at")
