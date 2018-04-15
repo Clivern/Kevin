878714c734dbe2d52befb4d21eb77e2add887ab5
Clivern <hello@clivern.com>
2018-04-15 15:33:14 +0200

    define all models in __init__ file

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,7 +3,9 @@ Load Models
 """
 
 from .endpoint import Endpoint
-from .endpoint_meta import EndpointMeta
+from .endpoint_meta import Endpoint_Meta
 from .namespace import Namespace
 from .request import Request
-from .request_meta import RequestMeta
+from .request_meta import Request_Meta
+from .profile import Profile
+from .option import Option
