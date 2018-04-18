8ef76bee6577a6f75bf60687c741005b87833a36
Clivern <hello@clivern.com>
2018-04-18 21:37:49 +0200

    Add endpoint entity module

--- app/modules/entity/endpoint_entity.py
+++ app/modules/entity/endpoint_entity.py
@@ -2,5 +2,11 @@
 Endpoint Entity Module
 """
 
+from app.models import Endpoint
+from app.models import Endpoint_Meta
+from django.contrib.auth.models import User
+from app.modules.util.helpers import Helpers
+
+
 class Endpoint_Entity():
     pass
