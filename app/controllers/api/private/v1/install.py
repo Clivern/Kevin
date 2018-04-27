82183bd2a9a10f9cc736ae70c50197df7aaff85b
Clivern <hello@clivern.com>
2018-04-28 00:25:43 +0200

    import form module in install controller

--- app/controllers/api/private/v1/install.py
+++ app/controllers/api/private/v1/install.py
@@ -7,13 +7,10 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from pprint import pprint
-from cerberus import Validator
-import json
+from app.modules.validation.form import Form
 
-#@method_decorator(csrf_exempt, name='dispatch')
+@method_decorator(csrf_exempt, name='dispatch')
 class Install(View):
 
     def post(self, request):
-        pprint(request.GET)
-        pprint(request.POST["hi"])
         return JsonResponse({"status":"d"})
