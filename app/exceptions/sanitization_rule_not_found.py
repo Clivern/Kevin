a81a761d2adea680d8ee6871881edb8123436c17
Clivern <hello@clivern.com>
2018-05-10 15:07:59 +0200

    Fix exceptions styling

--- app/exceptions/sanitization_rule_not_found.py
+++ app/exceptions/sanitization_rule_not_found.py
@@ -2,8 +2,10 @@
 Sanitization Rule Not Found
 """
 
+
 class Sanitization_Rule_Not_Found(Exception):
     """Sanitization Rule Not Exist Custom Exception"""
+
 
     def __init__(self, error_info):
         Exception.__init__(self, "Sanitization Rule Not Found!")
--- app/exceptions/validation_rule_not_found.py
+++ app/exceptions/validation_rule_not_found.py
@@ -2,8 +2,10 @@
 Validation Rule Not Found
 """
 
+
 class Validation_Rule_Not_Found(Exception):
     """Validation Rule Not Exist Custom Exception"""
+
 
     def __init__(self, error_info):
         Exception.__init__(self, "Validation Rule Not Found!")
