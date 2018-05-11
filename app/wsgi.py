fc8cb7988cc7226e2a0378f6ce70d97256e26c48
Clivern <hello@clivern.com>
2018-05-12 00:56:20 +0200

    wsgi docs updated

--- app/wsgi.py
+++ app/wsgi.py
@@ -4,6 +4,10 @@ WSGI Config for Kevin
 It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+
+To Run Using gunicorn
+pip install gunicorn
+gunicorn app.wsgi:application
 """
 
 # standard library
