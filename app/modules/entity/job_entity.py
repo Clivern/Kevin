6b0860f36eef73445c79dc53a3da08fcfac1dd4e
Clivern <hello@clivern.com>
2018-05-15 22:00:36 +0200

    add lock to jobs

--- app/modules/entity/job_entity.py
+++ app/modules/entity/job_entity.py
@@ -40,7 +40,8 @@ class Job_Entity():
             trials=job["trials"] if "trials" in job else 5,
             priority=job["priority"] if "priority" in job else 1,
             last_run=job["last_run"] if "last_run" in job else None,
-            run_at=job["run_at"] if "run_at" in job else self.get_run_at(job["interval"])
+            run_at=job["run_at"] if "run_at" in job else self.get_run_at(job["interval"]),
+            locked=job["locked"] if "locked" in job else True,
         )
         job.save()
         return False if job.pk is None else job
@@ -66,7 +67,11 @@ class Job_Entity():
     def get_one_to_run(self):
         """Get Job To Run"""
         try:
-            job = Job.objects.filter(status__in=["pending","daemon"], run_at__lt=timezone.now()).order_by("priority").first()
+            job = Job.objects.filter(status__in=["pending","daemon"], run_at__lt=timezone.now(), locked__eq=False).order_by("priority").first()
+            # Lock the Job
+            if not job.pk is None:
+                job.locked = True
+                job.save()
             return False if job.pk is None else job
         except:
             return False
@@ -98,6 +103,8 @@ class Job_Entity():
                 job.last_run = new_data["last_run"]
             if "run_at" in new_data:
                 job.run_at = new_data["run_at"]
+            if "locked" in new_data:
+                job.locked = new_data["locked"]
             job.save()
             return True
         return False
@@ -105,7 +112,7 @@ class Job_Entity():
 
     def update_after_run(self, job, status):
         """Update Job According To Status"""
-        job_data = {"last_run": timezone.now(), "retry_count": (job.retry_count + 1), "last_status": status}
+        job_data = {"last_run": timezone.now(), "retry_count": (job.retry_count + 1), "last_status": status, "locked": False}
 
         if job.status == "pending":
             if status == self.PASSED:
