8219e1501eafdaa172d53e0be0fbffa78832b0d4
Clivern <hello@clivern.com>
2018-06-02 02:07:52 +0200

    Analytics start from today

--- app/modules/core/statistics.py
+++ app/modules/core/statistics.py
@@ -57,7 +57,7 @@ class NamespacesStatistics():
 
         days_count = days_num
 
-        while days_count > -1:
+        while days_count > 0:
             current_date = timezone.now().date() - timedelta(days_count)
             current_date = str(current_date)
 
